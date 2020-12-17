f31ff1b40fa94171cd19049cd7b37f6e03a4eb8d
CherishYii <cherish@cherish.pw>
2020-12-17 18:04:22 +0800

    路由规划

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,24 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+
+// 导入路由模块
+import filmRouter from "./routes/films";
+import cinemaRouter from "./routes/cinemas";
+import centerRouter from "./routes/center";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    filmRouter,
+    cinemaRouter,
+    centerRouter,
+    { path: "/", redirect: "/films" },
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+});
 
-export default router
+export default router;
